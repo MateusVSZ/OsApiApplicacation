99a70f4daac9217b614870932c8550293137677a
MateusVSZ <mateusvieirasabino044@gmail.com>
2026-04-06 09:53:46 -0300

    Alteração da classe Entity - Add @NotBlank, NOT NULL - Add @Email, apenas ver se é um tipo email - Add @Size, tamaho dos caracteres - Add @Valid, nos métodos POST e PUT. Validação das anotações @

--- src/main/java/br/eti/mts/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/eti/mts/OSApiApplication/api/controller/ClienteController.java
@@ -5,35 +5,74 @@
 package br.eti.mts.OSApiApplication.api.controller;
 
 import br.eti.mts.OSApiApplication.domain.model.Cliente;
+import br.eti.mts.OSApiApplication.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.validation.Valid;
 import java.util.ArrayList;
 import java.util.List; //interface, a interface ela tem que fazer mas nao implementa, quem implementa é a classe
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author digma
  */
-
 @RestController
 public class ClienteController {
-    
-    @PersistenceContext
-    private EntityManager manager;   //Entity é o CRUD do banco a diferença que
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
     List<Cliente> listaClientes; //criei um objeto
-    
+
     @GetMapping("/clientes")
-    public List<Cliente> listas(){ //aqui criei um método que retorna lista<Cliente>
-     
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
-                                              
-        
- 
+    public List<Cliente> listas() { //aqui criei um método que retorna lista<Cliente>
+
+        return clienteRepository.findAll(); // é tudo
+
     }
-   }
 
-    
+    @GetMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteID) {// Long com L maiusculo é que clienteId não pode receber valor nulo
+        Optional<Cliente> cliente = clienteRepository.findById(clienteID); //pode ou não estar nulo
 
+        if (cliente.isPresent()) {
+
+            return ResponseEntity.ok(cliente.get());
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+
+    }
+
+    @PutMapping("clientes/{clienteID}")
+    public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteID, @RequestBody Cliente cliente) {
+
+        if (!clienteRepository.existsById(clienteID)) { //verifica se clienteID é diferente do id e retorna notfound
+
+            return ResponseEntity.notFound().build();
+
+        }
+        cliente.setId(clienteID); // set altera o id do parametro(clienteID)
+        cliente = clienteRepository.save(cliente);//aqui salva
+        return ResponseEntity.ok(cliente);//aqui retorna
+    }
+}
--- src/main/java/br/eti/mts/OSApiApplication/domain/model/Cliente.java
+++ src/main/java/br/eti/mts/OSApiApplication/domain/model/Cliente.java
@@ -4,12 +4,14 @@
  */
 package br.eti.mts.OSApiApplication.domain.model;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 /**
  *
@@ -21,24 +23,29 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank  // O atributo não pode ser nulo
+    @Email //verifica se é um email correto, mas não valida se existe
+    @Size(max = 255) // tamanho do caracter
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "telefone")
     private String fone;
 
     public Cliente() {
     }
 
-    public Cliente(long id, String nome, String email, String fone) {
-        this.id = id;
+    public Cliente(String nome, String email, String fone) {
         this.nome = nome;
         this.email = email;
         this.fone = fone;
     }
-    
-    
-        
 
     @Override
     public int hashCode() {
@@ -62,8 +69,6 @@ public class Cliente {
         return this.id == other.id;
     }
 
-        
-        
     public long getId() {
         return id;
     }
@@ -95,5 +100,5 @@ public class Cliente {
     public void setFone(String fone) {
         this.fone = fone;
     }
-        
+
 }
